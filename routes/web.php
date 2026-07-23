ad1f48ceac8f1f7b6e76bbd6abb941ba598c3e8e
Muhammed Nour Masry <mnmasry022@gmail.com>
2026-07-23 16:24:52 +0300

    Modify root route to return JSON API status
    Updated the root route to return a JSON response indicating the API status. Added routes for payment success and cancellation views.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Charity API is running',
+        'status' => 'ok',
+        'documentation' => url('/api'),
+    ]);
 });
+Route::get('/payment/success', function () {
+    return view('payment.success');
+})->name('payment.success');
+
+Route::get('/payment/cancel', function () {
+    return view('payment.cancel');
+})->name('payment.cancel');
